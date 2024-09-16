d99ac88676476e711189fae402cfa3868e653996
Pratik Sharma <sharma.pratik2016@gmail.com>
2024-09-16 15:15:48 +0530

    fix: final

--- apps/my-app/src/app/api/send/route.ts
+++ apps/my-app/src/app/api/send/route.ts
@@ -1,4 +1,6 @@
-import { EmailTemplate } from "@/emails/welcome";
+import * as React from "react";
+
+import { EmailTemplate } from "../../../emails/welcome";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.AUTH_RESEND_KEY || "");
@@ -9,7 +11,7 @@ export async function POST() {
       from: "Acme <onboarding@resend.dev>",
       to: ["sharma.pratik2016@gmail.com"],
       subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }),
+      react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
     });
 
     if (error) {
